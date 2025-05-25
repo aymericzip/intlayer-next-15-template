6653ace8680869207d3fd135c1bdbabeb4a80c31
Aymeric PINEAU <ay.pineau@gmail.com>
2025-05-25 23:40:37 +0200

    feat: update locale switcher

--- src/components/localeSwitcher/localeSwitcher.tsx
+++ src/components/localeSwitcher/localeSwitcher.tsx
@@ -1,34 +1,84 @@
 "use client";
 
-import { Locales } from "intlayer";
-import { useLocale } from "next-intlayer";
-import { FC } from "react";
+import { getHTMLTextDir, getLocaleName, getLocalizedUrl } from "intlayer";
+import { useIntlayer, useLocale, useLocaleCookie } from "next-intlayer";
+import Link from "next/link";
+import { useRef, useState, type FC } from "react";
+import { useLocaleSearch } from "./useLocaleSearch";
 
 export const LocaleSwitcher: FC = () => {
-  const { setLocale } = useLocale();
+  const { searchInput, localeSwitcherLabel } = useIntlayer("locale-switcher");
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const { locale, pathWithoutLocale, availableLocales } = useLocale();
+  const { setLocaleCookie } = useLocaleCookie();
+  const { searchResults, handleSearch } = useLocaleSearch(
+    availableLocales,
+    locale
+  );
 
   return (
-    <div className="flex flex-col items-start justify-start gap-4 text-left w-full">
+    <div
+      className="relative rounded-xl p-2 w-auto"
+      aria-label={localeSwitcherLabel.value}
+    >
       <button
-        className="text-blue-300 underline"
-        onClick={() => setLocale(Locales.ENGLISH)}
+        onClick={() => setIsOpen(!isOpen)}
+        className="flex w-full items-center justify-between cursor-pointer"
+        aria-expanded={isOpen}
       >
-        Switch to english
+        <span className="px-2">{getLocaleName(locale)}</span>
+        <span className="text-sm">▼</span>
       </button>
 
-      <button
-        className="text-blue-300 underline"
-        onClick={() => setLocale(Locales.FRENCH)}
-      >
-        Switch to french
-      </button>
-
-      <button
-        className="text-blue-300 underline "
-        onClick={() => setLocale(Locales.SPANISH)}
-      >
-        Switch to spanish
-      </button>
+      {isOpen && (
+        <div className="absolute right-0 top-full mt-1 min-w-60 rounded-xl bg-neutral-900 shadow-lg z-10">
+          <div className="p-3">
+            <input
+              type="search"
+              aria-label={searchInput.ariaLabel.value}
+              placeholder={searchInput.placeholder.value}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                handleSearch(e.target.value)
+              }
+              ref={inputRef}
+              className="w-full border rounded p-2"
+            />
+          </div>
+          <ul className="divide-y max-h-[80vh] overflow-y-auto backdrop-blur opacity-80">
+            {searchResults.map(
+              ({ locale: localeItem, currentLocaleName, ownLocaleName }) => (
+                <li key={localeItem} className="p-1">
+                  <Link
+                    href={getLocalizedUrl(pathWithoutLocale, localeItem)}
+                    className={`w-full flex flex-row items-center justify-between gap-3 text-left p-2 rounded-xl ${
+                      locale === localeItem
+                        ? "bg-neutral-800"
+                        : "cursor-pointer hover:bg-neutral-800"
+                    }`}
+                    onClick={() => {
+                      setLocaleCookie(localeItem);
+                      setIsOpen(false);
+                    }}
+                  >
+                    <div className="flex flex-col">
+                      <span dir={getHTMLTextDir(localeItem)} lang={localeItem}>
+                        {ownLocaleName}
+                      </span>
+                      <span className="text-gray-600 text-xs">
+                        {currentLocaleName}
+                      </span>
+                    </div>
+                    <span className="text-gray-600 text-sm">
+                      {localeItem.toUpperCase()}
+                    </span>
+                  </Link>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/components/localeSwitcher/useLocaleSearch.ts
@@ -0,0 +1,58 @@
+import Fuse, { type IFuseOptions } from "fuse.js";
+import { getLocaleName, Locales, type LocalesValues } from "intlayer";
+import { useState } from "react";
+
+type MultilingualAvailableLocales = {
+  locale: LocalesValues;
+  englishName: string;
+  currentLocaleName: string;
+  ownLocaleName: string;
+};
+
+export const useLocaleSearch = (
+  availableLocales: LocalesValues[] = Object.values(Locales),
+  locale: LocalesValues = Locales.ENGLISH
+) => {
+  const multilingualAvailableLocales: MultilingualAvailableLocales[] =
+    availableLocales.map((localeEl) => {
+      const englishName = getLocaleName(localeEl, Locales.ENGLISH);
+      const currentLocaleName = getLocaleName(localeEl, locale);
+      const ownLocaleName = getLocaleName(localeEl);
+      return {
+        locale: localeEl,
+        englishName,
+        currentLocaleName,
+        ownLocaleName,
+      };
+    });
+
+  const [searchResults, setSearchResults] = useState<
+    MultilingualAvailableLocales[]
+  >(multilingualAvailableLocales);
+
+  const fuseOptions: IFuseOptions<MultilingualAvailableLocales> = {
+    keys: [
+      { name: "ownLocaleName", weight: 0.4 },
+      { name: "englishName", weight: 0.2 },
+      { name: "currentLocaleName", weight: 0.2 },
+      { name: "locale", weight: 0.2 },
+    ],
+    threshold: 0.02, // Defines how fuzzy the matching should be (lower is more strict)
+  };
+  // Create a new Fuse instance with the options and documentation data
+  const fuse = new Fuse(multilingualAvailableLocales, fuseOptions);
+
+  const handleSearch = (searchQuery: string) => {
+    if (searchQuery) {
+      // Perform search on every input change
+      const searchResults = fuse
+        .search(searchQuery)
+        .map((result) => result.item);
+      setSearchResults(searchResults);
+    } else {
+      setSearchResults(multilingualAvailableLocales);
+    }
+  };
+
+  return { searchResults, handleSearch };
+};
