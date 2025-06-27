58ae3df7fc5e4e789c9a262478434ff96d78d43a
Aymeric PINEAU <ay.pineau@gmail.com>
2025-06-27 14:17:35 +0200

    feat: update metada example

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,37 @@
-import { NextLayoutIntlayer, generateStaticParams } from "next-intlayer";
+import {
+  LocalPromiseParams,
+  NextLayoutIntlayer,
+  generateStaticParams,
+} from "next-intlayer";
 import { Inter } from "next/font/google";
-import { getHTMLTextDir } from "intlayer";
+import {
+  getHTMLTextDir,
+  getIntlayer,
+  getTranslation,
+  IConfigLocales,
+  t,
+} from "intlayer";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const generateMetadata = async ({
+  params,
+}: LocalPromiseParams): Promise<Metadata> => {
+  const { locale } = await params;
+
+  const metadata = getIntlayer("page-metadata", locale);
+  const t = <T,>(content: IConfigLocales<T>) => getTranslation(content, locale);
+
+  return {
+    ...metadata,
+    title: t<string>({
+      en: `Intlayer | ${locale} | Demo`,
+      fr: `Intlayer | ${locale} | Démo`,
+      es: `Intlayer | ${locale} | Demostración`,
+    }),
+  };
+};
 
 const LocaleLayout: NextLayoutIntlayer = async ({ children, params }) => {
   const { locale } = await params;
